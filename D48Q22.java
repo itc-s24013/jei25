243dd22be41c8d7d1d52e418df4a3cea58d4c343
itc-s24013 <s24013@std.it-college.ac.jp>
2025-05-16 12:39:50 +0900

    第48回のやつ

---
+++ D48Q22.java
@@ -0,0 +1,11 @@
+enum Connection  {ONLINE,OFFLINE}
+
+public class D48Q22 {
+    public static void main(String[] args) {
+        Connection s = Connection.OFFLINE;
+        if (s == Connection.OFFLINE) {
+            s = Connection.ONLINE;
+        }
+        System.out.println(s);
+    }
+}
---
+++ D48Q27.java
@@ -0,0 +1,12 @@
+public class D48Q27 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 6){
+            System.out.println(i);
+            i++;
+        }
+    }
+    /*
+    「0」が繰り返し表示されるが、これを6までの数字を表示するように修正
+     */
+}
