52ef67fbc0e0f58617845dfe925b6c294f43fb9b
itc-s24013 <s24013@std.it-college.ac.jp>
2025-05-23 11:21:18 +0900

    2025/05/23_D49Q20課題

---
+++ D49Q20.java
@@ -0,0 +1,8 @@
+public class D49Q20 {
+    public static void main(String[] args) {
+        int [][] arr = new int[5][10];
+
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+    }
+}
