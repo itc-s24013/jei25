055c64944831b02a57d0266116cd43dd4351f2c8
itc-s24013 <s24013@std.it-college.ac.jp>
2025-05-30 10:34:21 +0900

    2025/05/30_D49Q27課題

---
+++ D49Q27.java
@@ -0,0 +1,10 @@
+public class D49Q27 {
+    public static void main(String[] args) {
+        int[] number = {10,20,30};
+        int sum = 0;
+        for (int num : number) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
