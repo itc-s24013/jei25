267370933ca42cf944eb3a26478af65f5c5a4e10
itc-s24013 <s24013@std.it-college.ac.jp>
2025-05-30 11:36:34 +0900

    2025/05/30_D50Q20課題

---
+++ D50Q20.java
@@ -0,0 +1,8 @@
+public class D50Q20 {
+    public static void main(String[] args) {
+        int num = 1234;
+        String scale = num > 1000 ? "Narrow" : "Wide";
+
+        System.out.println(scale);
+    }
+}
