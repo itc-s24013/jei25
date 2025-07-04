235ffcffeb15a2ac295232ba51a13e826901b21e
itc-s24013 <s24013@std.it-college.ac.jp>
2025-07-04 10:14:12 +0900

    2025/07/04_D51Q24kadai

---
+++ D51Q24.java
@@ -0,0 +1,9 @@
+public class D51Q24 {
+    public static void main(String[] args) {
+        for (int x = 1; x < 20; x++) {
+            if (x % 3 == 0 && x % 5 == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+}
