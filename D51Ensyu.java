a22e33634298c15b780749349c258c04a1939a90
itc-s24013 <s24013@std.it-college.ac.jp>
2025-07-11 10:55:39 +0900

    2025/07/11_D51ensyu

--- D51Ensyu.java
+++ D51Ensyu.java
@@ -5,3 +5,20 @@ public class D51Ensyu {
         System.out.println("氏名:" + student.getName());
     }
 }
+
+class D51Student {
+    private String name;
+    private String id;
+    public D51Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
