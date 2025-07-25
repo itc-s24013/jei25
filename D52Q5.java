5c45faa5f4184848a13c68602961b3bfddbb1956
itc-s24013 <s24013@std.it-college.ac.jp>
2025-07-25 10:51:14 +0900

    2025/07/18_D52Q5

--- D52Q5.java
+++ D52Q5.java
@@ -5,19 +5,19 @@ public class D52Q5 {
         if(args.length == 0) return;
         int max = 0, i = 0;
         int[] data = new int[args.length];
-        for(String s : args) {
-            data[i] = Integer.parseInt(s);
-            if(data[i] <= 0 || data[i] >= LIMIT) return;
-            if(data[i] > max) {
+        for(String s : args) { //argsをひとつずつ見るよ
+            data[i] = Integer.parseInt(s); //intに変換
+            if(data[i] <= 0 || data[i] >= LIMIT) return; //0以下か400以上だったら終わらせるよ
+            if(data[i] > max) { //最大値だったら入れ替えるよ
                 max = data[i];
             }
             i++;
         }
 
         //目盛り表示
-        int majormax = (max - 1) / 5 + i;
-        System.out.print("     ");
-        for (i = 0; i < majormax; i++) {
+        int majormax = (max - 1) / 10 + 1; //目盛りの長さを計算
+        System.out.print("       ");
+        for (i = 0; i < majormax; i++) { //iを初期化
             System.out.print(majors.charAt(i % 5));
         }
         System.out.println();
@@ -25,7 +25,7 @@ public class D52Q5 {
         //グラフ表示
         for(i = 0; i < data.length; i++) {
             System.out.printf("%4d : ", data[i]);
-            int n = (data[i] - 1) / 5 + 1;
+            int n = (data[i] - 1) / 10 + 1;
             for ( int j = 0; j < n; j++) {
                 System.out.print('*');
             }
