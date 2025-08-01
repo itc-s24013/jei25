5b7d312a679a886e806a236575e2ec4e7e889673
itc-s24013 <s24013@std.it-college.ac.jp>
2025-08-01 12:25:32 +0900

    2025/08/1_D53Q27kadai

---
+++ D53Q27.java
@@ -0,0 +1,22 @@
+enum Udon {
+    KAKE("かけ",290),KITUNE("きつね",-380),CURRY("カレー",490),ZARU("ざる",380);
+    private String japaneseName;
+    int price;
+    Udon(String japaneseName,int price) {
+        this.japaneseName = japaneseName;
+        if(price >= 0) {
+            this.price = price;
+        } else {
+            this.price = -1 * price;
+        }
+    }
+    public String toString() {
+        return japaneseName + ":" + price + "円";
+    }
+}
+
+public class D53Q27 {
+    public static void main(String[] args) {
+        System.out.println(Udon.ZARU);
+    }
+}
